5fd271495b1d43005f05dbd7b084635eb9b80b91
Kim Chang Wook <inter.kcw@interproindigo.com>
2022-01-25 16:47:24 +0900

    edit lint and format

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,10 @@
 <script>
-	import '../app.css';
+  import '../app.css'
 
-	import Navigation from '$lib/components/Navigation.svelte';
+  import Navigation from '$lib/components/Navigation.svelte'
 </script>
 
 <Navigation />
 <div class="container mx-auto mt-8 antialiased">
-	<slot />
+  <slot />
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,9 @@
 const config = {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [require('@tailwindcss/forms')],
-};
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('@tailwindcss/forms')]
+}
 
-module.exports = config;
+module.exports = config
